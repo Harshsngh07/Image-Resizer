2040bd6bb2f82e7a9a3168c4994c40d57d7cfae4
Harsh Singh <harshsngh07@gmail.com>
2020-06-12 21:17:06 +0530

    added nodemon

--- main.js
+++ main.js
@@ -1,1 +1,37 @@
-console.log("electron is here");
+const { app, BrowserWindow } = require("electron");
+
+//Set env
+process.env.NODE_ENV = "development";
+
+const isDev = process.env.NODE_ENV !== "production" ? true : false;
+const isMac = process.platform == "darwin" ? true : false;
+
+let mainWindow;
+
+function createMainWindow() {
+  mainWindow = new BrowserWindow({
+    title: "Image Resizer",
+    width: 500,
+    height: 600,
+    icon: "./assets/icons/compress-96.png",
+    resizable: isDev,
+  });
+
+  mainWindow.loadFile("./app/index.html");
+}
+
+app.on("ready", createMainWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
